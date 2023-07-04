45b20684b9010965465a2f2c834cdeeaef2cbd34
Steffanie Lopez <lopezsteffanie8@gmail.com>
2023-07-04 19:30:07 -0400

    First commit; practicing using Firebase with Java/Springboot

---
+++ src/test/java/com/stevieCodesIt/practiceFirebase/PracticeFirebaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stevieCodesIt.practiceFirebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeFirebaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
